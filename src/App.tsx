0926b63587700a603e10a7e27071d9081e961c47
João Vitor Zaniolo <54036572+jvzaniolo@users.noreply.github.com>
2022-01-14 17:36:09 -0300

    feat: create components
    - Header.tsx
    - SavingGoal.tsx
    - ReachDateProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { Header } from './layout/Header';
+import { SavingGoal } from './components/SavingGoal';
+import { ReachDateProvider } from './contexts/ReachDateContext';
 
 export function App(): JSX.Element {
-  return <div data-testid="greetings-container">Welcome to the Origin THA</div>;
+  return (
+    <div className="w-screen h-screen bg-background">
+      <Header />
+      <main className="flex justify-center">
+        <ReachDateProvider>
+          <SavingGoal />
+        </ReachDateProvider>
+      </main>
+    </div>
+  );
 }

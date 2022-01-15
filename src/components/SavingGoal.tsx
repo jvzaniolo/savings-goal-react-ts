7274e1be822cce3cbcf551647861ebb532d6ea52
João Vitor Zaniolo <54036572+jvzaniolo@users.noreply.github.com>
2022-01-14 23:39:54 -0300

    feat: new implementation with react currency input

--- src/components/SavingGoal.tsx
+++ src/components/SavingGoal.tsx
@@ -1,26 +1,30 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { useReachDate } from '../contexts/ReachDateContext';
 import { ReachDate } from './ReachDate';
-import { formatCurrency } from '../utils/currency';
-import { getMonthlyDeposits } from '../core/savingGoalCore';
+import { MonthlyAmount } from './MonthlyAmount';
+import { CurrencyInput } from '../layout/CurrencyInput';
+import { BsCurrencyDollar } from 'react-icons/bs';
 
 import buyAHouseImg from '../assets/icons/buy-a-house.svg';
 
-export function SavingGoal(): JSX.Element {
-  const [amount, setAmount] = useState(0);
-  const { month, monthIndex, year } = useReachDate();
-  const monthlyDeposits = getMonthlyDeposits(monthIndex);
+interface Amount {
+  float: number;
+  formatted: string;
+  value: string;
+}
 
-  function handleAmountChange(event: ChangeEvent<HTMLInputElement>) {
-    setAmount(parseInt(event.target.value));
-  }
+export function SavingGoal(): JSX.Element {
+  const [amount, setAmount] = useState<Amount | undefined>(undefined);
+  const { month, year, monthlyDeposits } = useReachDate();
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
   }
 
-  function handleMonthlyAmount() {
-    return formatCurrency(amount > 0 ? amount / monthlyDeposits : 0);
+  function handleMonthlyAmount(): number | undefined {
+    if (!amount) return;
+
+    return amount.float > 0 ? amount.float / monthlyDeposits : 0;
   }
 
   return (
@@ -30,68 +34,47 @@ export function SavingGoal(): JSX.Element {
         <strong className="font-semibold">saving goal.</strong>
       </h3>
 
-      <div className="max-w-xl pt-8 p-10 bg-neutral-white rounded-lg shadow-level4">
-        <form className="flex flex-wrap space-y-6" onSubmit={handleSubmit}>
+      <div className="sm:w-[40rem] pt-8 p-10 bg-neutral-white rounded-lg shadow-level4">
+        <form
+          className="flex flex-col flex-wrap space-y-6"
+          onSubmit={handleSubmit}
+        >
           <section className="flex space-x-4">
             <img src={buyAHouseImg} alt="Buy a house" />
             <div className="space-y-1">
-              <h1 className="text-2xl font-medium text-blue-gray-900">
+              <h1 className="text-2xl font-display font-medium text-blue-gray-900">
                 Buy a house
               </h1>
               <h2 className="text-blue-gray-400">Saving Goal</h2>
             </div>
           </section>
 
-          <div className="w-full flex flex-wrap gap-4 items-end">
-            <div className="flex flex-col flex-1">
-              <label
-                htmlFor="amount"
-                className="mb-1 text-sm text-blue-gray-900"
-              >
-                Total amount
-              </label>
-              <input
+          <div className="flex flex-wrap gap-4">
+            <div className="flex-1">
+              <CurrencyInput
                 id="amount"
                 type="text"
-                value={amount}
-                onChange={handleAmountChange}
-                className="flex-1 p-3 text-2xl rounded border border-blue-gray-50 text-blue-gray-600"
+                label="Total amount"
+                icon={<BsCurrencyDollar size={24} />}
+                onValueChange={(_, __, values) => setAmount(values as Amount)}
               />
             </div>
 
-            <div className="flex flex-col flex-1">
+            <div className="flex-1">
               <span className="mb-1 text-sm text-blue-gray-900">
                 Reach goal by
               </span>
-              <ReachDate className="max-h-[58px] min-w-[190px]" />
+              <ReachDate className="" />
             </div>
           </div>
 
-          <div className="flex flex-col rounded-lg border border-blue-gray-50">
-            <p className="flex justify-between py-6 px-8 items-center">
-              <span className="text-xl text-blue-gray-900">Monthly amount</span>
-              <span className="text-[32px] text-brand-secondary font-bold">
-                {handleMonthlyAmount()}
-              </span>
-            </p>
-
-            <div className="py-6 px-8 bg-blue-gray-10">
-              <span className="text-blue-gray-900 text-sm">
-                You&apos;re planning{' '}
-                <strong className="font-semibold">
-                  {monthlyDeposits} monthly deposits
-                </strong>
-                &nbsp;to reach your{' '}
-                <strong className="font-semibold">
-                  {formatCurrency(amount)}
-                </strong>{' '}
-                goal by&nbsp;
-                <strong className="font-semibold">
-                  {month} {year}.
-                </strong>
-              </span>
-            </div>
-          </div>
+          <MonthlyAmount
+            month={month}
+            year={year}
+            amount={amount?.float}
+            monthlyAmount={handleMonthlyAmount()}
+            monthlyDeposits={monthlyDeposits}
+          />
 
           <button
             type="submit"

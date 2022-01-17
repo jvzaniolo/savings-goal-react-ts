c26f1f91edeb69122843a675cc3f3a97ccfb8a6a
João Vitor Zaniolo <54036572+jvzaniolo@users.noreply.github.com>
2022-01-17 02:44:32 -0300

    refact: change context to hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { Header } from './layout/Header';
 import { SavingGoal } from './components/SavingGoal';
-import { SavingGoalProvider } from './contexts/SavingGoalContext';
 
 export function App(): JSX.Element {
   return (
     <div className="w-screen h-screen overflow-auto bg-background">
       <Header />
       <main className="mt-8 sm:mt-12 flex flex-col items-center">
-        <SavingGoalProvider>
-          <SavingGoal />
-        </SavingGoalProvider>
+        <SavingGoal />
       </main>
     </div>
   );

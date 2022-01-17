274f2fb9f196dce7c7a221383d09518126932af1
João Vitor Zaniolo <54036572+jvzaniolo@users.noreply.github.com>
2022-01-17 02:51:26 -0300

    test(header): remove snapshot test

--- src/layout/Header.test.tsx
+++ src/layout/Header.test.tsx
@@ -2,11 +2,6 @@ import { Header } from './Header';
 import { render, screen } from '@testing-library/react';
 
 describe('Header', () => {
-  it('should render the Header component', () => {
-    const component = render(<Header />);
-    expect(component).toMatchSnapshot();
-  });
-
   it('should render the brand logo', () => {
     render(<Header />);
 

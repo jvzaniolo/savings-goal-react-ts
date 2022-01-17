f6110451594bd56438874e1d31a774583f73ef7e
João Vitor Zaniolo <54036572+jvzaniolo@users.noreply.github.com>
2022-01-17 02:47:44 -0300

    refact: better typings and function names

--- src/components/SavingGoal/ReachDateInput.tsx
+++ src/components/SavingGoal/ReachDateInput.tsx
@@ -2,44 +2,37 @@ import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 import {
   forwardRef,
   ForwardRefRenderFunction,
-  InputHTMLAttributes,
   ReactNode,
   useEffect,
   useState,
 } from 'react';
 
-interface ReachDateInputComponentProps
-  extends InputHTMLAttributes<HTMLInputElement> {
-  month: string;
-  year: number;
+interface ReachDateInputComponentProps {
+  id: string;
+  className?: string;
+  value: {
+    month: string;
+    year: number;
+  };
   label?: ReactNode;
   isDisabled: boolean;
-  onPrevMonthChange(): void;
-  onNextMonthChange(): void;
+  onMonthDecrease(): void;
+  onMonthIncrease(): void;
 }
 
 const ReachDateInputComponent: ForwardRefRenderFunction<
   HTMLInputElement,
   ReachDateInputComponentProps
 > = (
-  {
-    id,
-    className,
-    label,
-    month,
-    year,
-    isDisabled,
-    onPrevMonthChange,
-    onNextMonthChange,
-  },
+  { id, className, label, value, isDisabled, onMonthDecrease, onMonthIncrease },
   ref
 ) => {
   const [hasFocus, setHasFocus] = useState(false);
 
   useEffect(() => {
     function eventListener(event: KeyboardEvent) {
-      if (event.key === 'ArrowRight') onNextMonthChange();
-      if (event.key === 'ArrowLeft') onPrevMonthChange();
+      if (event.key === 'ArrowLeft') onMonthDecrease();
+      if (event.key === 'ArrowRight') onMonthIncrease();
     }
 
     if (hasFocus) {
@@ -47,7 +40,7 @@ const ReachDateInputComponent: ForwardRefRenderFunction<
     }
 
     return () => window.removeEventListener('keydown', eventListener);
-  }, [hasFocus, onPrevMonthChange, onNextMonthChange]);
+  }, [hasFocus, onMonthDecrease, onMonthIncrease]);
 
   return (
     <div className="flex flex-1 flex-col">
@@ -65,7 +58,7 @@ const ReachDateInputComponent: ForwardRefRenderFunction<
           <button
             type="button"
             tabIndex={2}
-            onClick={onPrevMonthChange}
+            onClick={onMonthDecrease}
             className={`w-10 h-10 text-center rounded-full transition-colors focus:outline-none ${
               isDisabled
                 ? 'text-blue-gray-50 hover:bg-transparent cursor-not-allowed'
@@ -79,15 +72,15 @@ const ReachDateInputComponent: ForwardRefRenderFunction<
           </button>
           <section className="flex flex-col flex-1 items-center text-sm sm:text-base">
             <span className="w-24 font-semibold text-center text-blue-gray-800">
-              {month}
+              {value.month}
             </span>
 
-            <span className="text-blue-gray-400">{year}</span>
+            <span className="text-blue-gray-400">{value.year}</span>
           </section>
           <button
             type="button"
             tabIndex={3}
-            onClick={onNextMonthChange}
+            onClick={onMonthIncrease}
             className="flex items-center text-center rounded-full transition-colors focus:outline-none text-blue-gray-300 hover:bg-slate-100 "
             onFocus={() => setHasFocus(true)}
             onBlur={() => setHasFocus(false)}
@@ -102,10 +95,12 @@ const ReachDateInputComponent: ForwardRefRenderFunction<
         id={id}
         name={id}
         className="absolute -left-full opacity-0"
-        defaultValue={`${month} ${year}`}
+        value={`${value.month},${value.year}`}
         onFocus={() => setHasFocus(true)}
         onBlur={() => setHasFocus(false)}
         data-testid="hidden-input"
+        // fix for property value without onChange function
+        readOnly
       />
     </div>
   );
